c07e506c76570bdd03a6940fc5f453a85b5e52c1
DSL0680 <ekthf988172@gmail.com>
2024-12-02 16:29:24 +0900

    chore: url edit

--- fastAPI.py
+++ fastAPI.py
@@ -60,7 +60,7 @@ def load_image_embeddings():
 # 이미지 임베딩을 로드
 load_image_embeddings()
 
-SPRING_SERVER_URL = "https://3.34.191.185:8080/api/v1/saveState"
+SPRING_SERVER_URL = "https://esnack24.store/api/v1/saveState"
 
 # 유사 이미지 검색
 @app.post("/search")
