6cade44eaded1b05dffb42f363fa92496eeec2e8
DSL0680 <ekthf988172@gmail.com>
2024-12-02 17:18:03 +0900

    chore: url edit

--- fastAPI.py
+++ fastAPI.py
@@ -13,8 +13,8 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://esnack24.store"],
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_origins=["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
 )
